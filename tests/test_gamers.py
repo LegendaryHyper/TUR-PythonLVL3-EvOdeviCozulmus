65330697e6328e5b5bdbeca584d6418becc49176
GitKodland <125306926+GitKodland@users.noreply.github.com>
2025-04-28 11:55:57 +0300

    Update test_gamers.py

--- tests/test_gamers.py
+++ tests/test_gamers.py
@@ -3,23 +3,23 @@ from gamers import Gamer
 
 
 def test_Gorev_1_ad_ve_yas_kontrol():
-    gamer = Oyuncu("Emre", 14, "PrensEmre", "emre@gmail.com")
+    gamer = Gamer("Emre", 14, "PrensEmre", "emre@gmail.com")
     assert hasattr(gamer, 'name'), "Görev 1 tamamlanmadı. 'Gamer' sınıfı 'name' niteliğini içermiyor"
     assert hasattr(gamer, 'age'), "Görev 1 tamamlanmadı. 'Gamer' sınıfı 'age' niteliğini içermiyor"
 
 
 def test_Gorev_2_ekle_takmaad():
-    gamer = Oyuncu("Emre", 14, "PrensEmre", "emre@gmail.com")
+    gamer = Gamer("Emre", 14, "PrensEmre", "emre@gmail.com")
     assert hasattr(gamer, 'nickname'), "Görev 2 tamamlanmadı. 'Gamer' sınıfı 'nickname' niteliğini içermiyor"
 
 
 def test_Gorev_3_ekle_eposta():
-    gamer = Oyuncu("Emre", 14, "PrensEmre", "emre@gmail.com")
+    gamer = Gamer("Emre", 14, "PrensEmre", "emre@gmail.com")
     assert hasattr(gamer, 'email'), "Görev 3 tamamlanmadı. 'Gamer' sınıfı 'email' niteliğini içermiyor"
 
 
 def test_Task_4_degistir_mesaj(capsys):
-    gamer = Oyuncu("Emre", 14, "PrensEmre", "emre@gmail.com")
+    gamer = Gamer("Emre", 14, "PrensEmre", "emre@gmail.com")
     gamer.introduce()
     captured = capsys.readouterr()
     expected_output = "Merhaba, benim adım Emre, 14 yaşındayım. Bana her zaman emre@gmail.com adresine eposta göndererek ulaşabilirsiniz. Beni oyunda PrensEmre takma adıyla arayın."
